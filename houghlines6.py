da43464d576503c05840c8a1e904c15461eff453
snakazawa <nakazawa.tms0314@gmail.com>
2016-10-17 13:14:26 +0900

    refactor hoguhline6

--- houghlines6.py
+++ houghlines6.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 30 13:47:25 2016
-
-@author: s1321168
-"""
 
 import cv2
 import numpy as np
@@ -11,78 +6,67 @@ import matplotlib.pyplot as plt
 from matplotlib import interactive
 
 X_THRESHOLD = 20  # px
-Y_THRESHOLD = 50 # px
+Y_THRESHOLD = 50  # px
 
 interactive(True)
 
 
-def myhough(threshold, minLineLength, maxLineGap):
-    img = cv2.imread("./images/book.jpg")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 100, 150, apertureSize=3)
-
-    h = len(edges)
-    w = len(edges[0])
-    tw = 2  # px
+def countBlack(ary, tw=2):
+    h, w = ary.shape[:2]
     minh = int(h / 3 * 1)
-    maxh = int(h / 3 * 2)
-    threshold2 = 70
+    maxh = int(w / 3 * 2)
     cnts = []
     for sx in range(0, w - tw):
         cnt = 0
         for y in range(minh, maxh):
-            if any([edges[y][x] == 255 for x in range(sx, sx + tw)]):
+            if any([ary[y][x] == 255 for x in range(sx, sx + tw)]):
                 cnt += 1
         cnts.append(cnt)
+    return cnts
 
-    # detection peak
+
+def detectPeek(ary, pw=20):
     peeks = []
-    pw = 20
-    peek_v = 200
-    for i in range(len(cnts) - pw):
-        if all([cnts[i - j] <= cnts[i] for j in range(-pw, pw + 1) if j != 0]):
-            peeks.append(peek_v)
-        else:
-            peeks.append(0)
+    for i in range(len(ary) - pw):
+        if all([ary[i - j] <= ary[i] for j in range(-pw, pw + 1) if j != 0]):
+            peeks.append(i)
+    return peeks
 
 
-    plt.plot(range(0, len(cnts)), cnts)
-    plt.plot(range(0, len(peeks)), peeks)
-    plt.show()
+def myhough(threshold, minLineLength, maxLineGap):
+    img = cv2.imread("./images/book.jpg")
 
+    h, w = img.shape[:2]
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 100, 150, apertureSize=3)
     cv2.imshow("canny", edges)
 
-    for sx in range(0, w - tw):
-        if cnts[sx] < threshold2:
-            for y in range(0, h):
-                for x in range(sx, sx + tw):
-                    edges[y][x] = 0
+    cnts = countBlack(edges, tw=2)
+    plt.plot(range(0, len(cnts)), cnts)
 
-    cv2.imshow("canny2", edges)
+    peeks = detectPeek(cnts, pw=20)
+    plt.plot(peeks, [cnts[x] for x in peeks], 'o')
+    plt.show()
 
+    # hough
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold, minLineLength=minLineLength, maxLineGap=maxLineGap)
 
     if lines is None:
         print('lines not found')
         return
 
-    # hough lines
-    # for line in lines:
-    #     x1, y1, x2, y2 = line[0]
-    #     if abs(x1 - x2) < X_THRESHOLD / 180 * np.pi and abs(y1 - y2) > Y_THRESHOLD:
-    #         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # draw hough lines only vertically line
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        if abs(x1 - x2) < X_THRESHOLD / 180 * np.pi and abs(y1 - y2) > Y_THRESHOLD:
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    # peek lines
-    for i in range(len(peeks)):
-        if peeks[i] == peek_v:
-            cv2.line(img, (i, 0), (i, h), (0, 0, 255), 2)
+    # draw peek lines
+    for w in peeks:
+        cv2.line(img, (w, 0), (w, h), (0, 0, 255), 2)
 
     cv2.imshow("threshold:{0}, minLineLength:{1}, maxLineGap:{2}".format(threshold, minLineLength, maxLineGap), img)
-
-# for t in range(0, 100, 20):
-#     for mlen in range(0, 200, 40):
-#         for mgap in range(0, 30, 10):
-#             myhough(idx, t, mlen, mgap)
 
 myhough(10, 10, 30)
 
